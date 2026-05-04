1d187dabeada27d9febe37d2a03b270ad3d8b124
Abhishek <pundirabhishek987@gmail.com>
2026-05-04 12:49:37 +0530

    Problem1 solution

---
+++ NodeJS_Day2_Problem_Solving/index.js
@@ -0,0 +1,3 @@
+console.log("Assigment solution...");
+//The purpose of package.json file is to manage the depedencies of the project and to provide information about the project such as name, version, description, author, etc. It also allows our project to be easily shared and installed by others. It is a crucial file for any Node.js project as it helps to keep track of the packages and libraries that the proect relies on, making it easier to maintain and update the project in the future.
+
