adca09109c36d45c220d6ad6f946d61708a8ef42
Abhishek <pundirabhishek987@gmail.com>
2026-05-04 13:09:10 +0530

    Problem_Solution

--- NodeJS_Day2_Problem_Solving/index.js
+++ NodeJS_Day2_Problem_Solving/index.js
@@ -4,14 +4,47 @@ console.log("Assigment solution...");
 
 //Problem 2: Create a method to calculate the area of a circle
 
-const calculatreAreaOfCircle = (radius) => {
-  if (radius < 0) {
-    return "Radius cannot be negative";
+// const calculatreAreaOfCircle = (radius) => {
+//   if (radius < 0) {
+//     return "Radius cannot be negative";
+//   }
+//     const area = Math.PI * Math.pow(radius, 2);
+//     return area;
+// };
+
+// const result = calculatreAreaOfCircle(5);
+// console.log(`The area of the circle with radius 5 is: ${result}`);
+
+
+//Problem 3 : 
+
+const performOperation = (num1, num2, operation) => {
+  switch (operation) {
+    case "add":
+      return num1 + num2;
+    case "subtract":
+      return num1 - num2;
+    case "multiply":
+      return num1 * num2;
+    case "divide":
+      if (num2 === 0) {
+        return "Error: Division by zero is not allowed.";
+      }
+      return num1 / num2;
+    default:
+      return "Error: Invalid operation.";
   }
-    const area = Math.PI * Math.pow(radius, 2);
-    return area;
-};
+};      
 
-const result = calculatreAreaOfCircle(5);
-console.log(`The area of the circle with radius 5 is: ${result}`);
-
+const result1 = performOperation(10, 5, "add"); // 15` 
+const result2 = performOperation(10, 5, "subtract"); // 5
+const result3 = performOperation(10, 5, "multiply"); // 50
+const result4 = performOperation(10, 5, "divide"); // 2
+const result5 = performOperation(10, 0, "divide"); // "Error: Division by zero is not allowed."
+const result6 = performOperation(10, 5, "modulus"); // "Error: Invalid operation."      
+console.log(`Addition: ${result1}`);
+console.log(`Subtraction: ${result2}`);
+console.log(`Multiplication: ${result3}`);      
+console.log(`Division: ${result4}`);
+console.log(`Division by zero: ${result5}`);
+console.log(`Invalid operation: ${result6}`);
